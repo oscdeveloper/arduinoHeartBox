3bd60927d3ad9f6361527e69d1bf4b782395765a
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2017-07-03 03:05:11 +0200

    add comments and change variables names

--- heart-box.ino
+++ heart-box.ino
@@ -1,55 +1,78 @@
-int ledPin = 9;   
-int delayTime = 50;
-int fadeFrame = 1;
-int maxBrightness = 30;
-int minBrightness = 1;
+int heartLedsTransistorPin = 9;
+int heartDelayTime = 50;
+int heartFadeFrame = 1;
+int heartMaxBrightness = 30;
+int heartMinBrightness = 1;
 
-int button = 3;
-boolean buttonAlreadyPressed = false;
+int ringButtonPin = 3;
+int ringLedPin = 2;
 
-int ringLed = 2;
-int mirrorLeftLed = 5;
-int mirrorRightLed = 6;
-int mirrorLedBrightness = 255;
+int mirrorLeftLedPin = 5;
+int mirrorRightLedPin = 6;
+int mirrorLedsMaxBrightness = 255;
+int mirrorLedsMinBrightness = 0;
+int mirrorLedsFadeFrame = 1;
+
+
+
 
 void setup() {
-  
-  //Serial.begin(9600);
 
-  pinMode(button, INPUT_PULLUP);
-  pinMode(ringLed, OUTPUT);
+  pinMode(ringButtonPin, INPUT_PULLUP);
+  pinMode(ringLedPin, OUTPUT);
+  pinMode(mirrorLeftLedPin, OUTPUT);
+  pinMode(mirrorRightLedPin, OUTPUT);
   
 }
 
+
+
+
 void loop() {
 
-  if ( digitalRead(button) == HIGH ) {
+  if ( digitalRead(ringButtonPin) == HIGH ) { // box is open, heart stops pulsing
 
-    analogWrite(ledPin, 0);
-    
-    digitalWrite(ringLed, HIGH);
-    analogWrite(mirrorLeftLed, mirrorLedBrightness);
-    analogWrite(mirrorRightLed, mirrorLedBrightness);
-    
-  } else {
-    
-    digitalWrite(ringLed, LOW);
-    analogWrite(mirrorLeftLed, 0);
-    analogWrite(mirrorRightLed, 0);
+    // stop heart animation
+    analogWrite(heartLedsTransistorPin, 0);
 
-    for (int fadeValue = minBrightness; fadeValue <= maxBrightness; fadeValue += fadeFrame) {
-      analogWrite(ledPin, fadeValue);
-      //Serial.println(fadeValue);
-      delay(delayTime);
+    // pause for 1sec, to have time to fully open the box ;)
+    delay(1000);
+
+    // mirror leds fade in
+    for (int fadeValue = mirrorLedsMinBrightness; fadeValue <= mirrorLedsMaxBrightness; fadeValue += mirrorLedsFadeFrame) {
+      analogWrite(mirrorLeftLedPin, fadeValue);
+      analogWrite(mirrorRightLedPin, fadeValue);      
     }
-  
-    for (int fadeValue = maxBrightness; fadeValue >= minBrightness; fadeValue -= fadeFrame) {
-      analogWrite(ledPin, fadeValue);
-      //Serial.println(fadeValue);
-      delay(delayTime);
+
+    // led in ring box is on
+    digitalWrite(ringLedPin, HIGH);
+
+  // box is closed, heart starts pulsing        
+  } else {
+
+    // led in ring box is off
+    digitalWrite(ringLedPin, LOW);
+
+    // swtich off mirror leds
+    analogWrite(mirrorLeftLedPin, mirrorLedsMinBrightness);
+    analogWrite(mirrorRightLedPin, mirrorLedsMinBrightness);
+
+    // heart pulsing animation, fade in
+    for (int fadeValue = heartMinBrightness; fadeValue <= heartMaxBrightness; fadeValue += heartFadeFrame) {
+      analogWrite(heartLedsTransistorPin, fadeValue);
+      delay(heartDelayTime);
+    }
+
+    // heart pulsing animation, fade out
+    for (int fadeValue = heartMaxBrightness; fadeValue >= heartMinBrightness; fadeValue -= heartFadeFrame) {
+      analogWrite(heartLedsTransistorPin, fadeValue);
+      delay(heartDelayTime);
     }
       
   }
+
+  // pause 100ms, it's not necessary
+  delay(100);
   
 }
 
